b560c52e3347728fd1109eeeddc3482b8057cd94
Aoyagi Satoshi <aoyagi@example.com>
2025-07-04 21:47:04 +0900

    second commit

--- coding_advanced/html/site.js
+++ coding_advanced/html/site.js
@@ -1,11 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("hamburger").addEventListener("click", function () {
-    document.getElementById("sumaho-menu").classList.toggle("show");
+
+
+$(function () {
+  $('#menu-btn-check').on('click', function () {
+    const menu = $('#sp-menu');
+    const search= $('#sp-search');
+    console.log('ボタンがクリックされました');
+    if (menu.is(':visible')) {
+      menu.slideUp();
+      search.slideUp();
+    } else {
+      menu.slideDown();
+      search.slideDown();
+    }
   });
+});
 
   const swiper = new Swiper(".swiper", {
-    slidesPerView: 2,
     loop: true,
+    slidesPerView: 1,
+     breakpoints: {
+    // スライドの表示枚数：600px以上の場合
+    600: {slidesPerView: 2,},},
     speed: 2000,
     autoplay: {
       delay: 1000,
@@ -18,8 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
-
+    scrollbar: {
+      el: '.swiper-scrollbar',
+  },
+});
   swiper.autoplay.stop();
 
   window.addEventListener("scroll", function () {
@@ -33,9 +50,28 @@ document.addEventListener("DOMContentLoaded", function () {
       swiper.autoplay.stop();
     }
   });
-});
+
+
 $(function () {
     $('.news-topics').on('click', function () {
     $(this).next().slideToggle();
+  });
+});
+
+$(function(){
+  $(".inview").on("inview", function (event, isInView) {
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+    }
+  });
+});
+
+$(function(){
+  $(".inview").on("inview", function (event, isInView) {
+    console.log("テスト", this, isInView);
+    if (isInView) {
+      $(this).stop().addClass("is-show");
+      console.log("is-show加えた");
+    }
   });
 });
